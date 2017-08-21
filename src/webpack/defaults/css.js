8212364eeb8b23092bb7a2a085b449ad94aa1d5f
Vaughan Rouesnel <vrouesnel@gmail.com>
2017-08-21 18:20:08 +0200

    flexboxgrid

--- src/webpack/defaults/css.js
+++ src/webpack/defaults/css.js
@@ -24,11 +24,26 @@ export default function(webpack, opts, config) {
     name: 'css-loader',
     test: /\.css$/,
     // TODO(vjpr): Do we need to ignore node_modules?
-    exclude: [/node_modules\/react-toolbox/, /modules\/react-toolbox-config/],
+    exclude: [
+      /node_modules\/react-toolbox/,
+      /modules\/react-toolbox-config/,
+      /flexboxgrid/,
+    ],
     queries: [
       [styleLoaderName, {}],
       ['css-loader', cssOpts],
       //['postcss-loader', {config: {path: join(process.cwd(), 'tools/postcss.config.js')}}],
+    ]
+  }, parseStyleLoaders({useExtractTextPlugin: opts.useExtractTextPlugin}))
+
+  config.loader('pcss', {
+    name: 'postcss-loader',
+    test: /\.pcss$/,
+    // TODO(vjpr): Do we need to ignore node_modules?
+    queries: [
+      [styleLoaderName, {}],
+      ['css-loader', cssOpts],
+      ['postcss-loader', {config: {path: join(process.cwd(), 'tools/postcss.config.js')}}],
     ]
   }, parseStyleLoaders({useExtractTextPlugin: opts.useExtractTextPlugin}))
 
@@ -44,5 +59,16 @@ export default function(webpack, opts, config) {
       ['postcss-loader', {config: {path: join(process.cwd(), 'tools/postcss.config.js')}}],
     ]
   }, parseStyleLoaders({useExtractTextPlugin: opts.useExtractTextPlugin}))
+
+  //config.loader('flexbox css', {
+  //  name: 'css-loader',
+  //  test: /\.css$/,
+  //  // TODO(vjpr): Do we need to ignore node_modules?
+  //  include: [/flexboxgrid/],
+  //  queries: [
+  //    [styleLoaderName, {}],
+  //    ['css-loader', {...cssOpts, module: true}],
+  //  ]
+  //}, parseStyleLoaders({useExtractTextPlugin: opts.useExtractTextPlugin}))
 
 }
